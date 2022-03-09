388e1607b1d8933d151ea2ac47cae1767d2cff22
TejasFirodiya <TejasFirodiya@WAIANGDESK14>
2022-03-09 17:59:05 +0530

    Add project files.

---
+++ Day3/DemoDay03Assignment.cs
@@ -0,0 +1,68 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class DemoDay03Assignment
+{
+
+    public static void Test()
+    {
+        Console.WriteLine("===========Assignment Day03==========");
+
+        Console.Write("Enter an equation containing numbers and operators : ");
+        string str = Console.ReadLine();
+        string[] strarr = str.Split(" ");
+
+        int ans = int.Parse(strarr[0]); // 5 + 5 + 5 + 5 
+
+        for (int i = 1; i < strarr.Length; i++) 
+        {
+            if (strarr[i] == "+")
+            {
+                ans += int.Parse(strarr[i + 1]);
+                i++;
+            }
+            else if (strarr[i] == "-")
+            {
+                ans -= int.Parse(strarr[i + 1]);
+                i++;
+            }
+            else if (strarr[i] == "*")
+            {
+                ans *= int.Parse(strarr[i + 1]);
+                i++;
+            }
+            else if (strarr[i] == "/")
+            {
+                ans /= int.Parse(strarr[i + 1]);
+                i++;
+            }
+            else
+            {
+                ans = int.Parse(strarr[i]); 
+                i++;
+            }
+            //BODMAS(int.Parse(strarr[i]));
+
+        }
+        Console.WriteLine("Answer : " + ans);
+    }
+
+    //public static void BODMAS(string[] strarr)
+    //{
+    //    int ans = int.Parse(strarr[0]);
+
+    //    for(int i = 1; i < strarr.Length; i++)
+    //    {
+    //        if(strarr[i] == "*")
+    //        {
+    //            ans *= int.Parse(strarr[i]);
+    //            Console.Write(ans);
+    //        }
+    //        else if (strarr[i] == "/")
+    //        {
+    //            ans /= int.Parse(strarr[i]);
+    //            Console.Write(ans);
+    //        }
+
+    //    }
+    //}
+}
---
+++ Day3/DemoEscapeSequence.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class DemoEscapeSequence
+{
+    internal static void Test()
+    {
+        Console.WriteLine("Escape sequence for single quote : \'");
+        Console.WriteLine("Escape sequence for Double quote : \"");
+        Console.WriteLine("Escape sequence for Backslash : \\ ");
+        Console.WriteLine("Escape sequence for Null : \0sd");
+        Console.WriteLine("Escape sequence for Alert : \a");
+        Console.WriteLine("Escape sequence for Backspace : \bcsdsf");
+        Console.WriteLine("Escape sequence for Form feed : \fdfs");
+        Console.WriteLine("Escape sequence for new Line : \n");
+        Console.WriteLine("Escape sequence for Carriage return : \rtea");
+        Console.WriteLine("Escape sequence for Horizontal tab : \t");
+    }
+
+   
+}
+
+
+
